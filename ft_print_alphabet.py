0198b0c35c0dde8c50ef4275d8741bc9d8d379b7
Taha Akdoğan <r.taha_akdogan@outlook.com>
2023-06-28 00:35:53 +0300

    ft_print_alphabet

---
+++ ft_print_alphabet.py
@@ -0,0 +1,8 @@
+
+c = ord('a')
+#ord('a') ifadesi 'a' karakterinin Unicode değerini döndürür.
+#'a' karakterinin Unicode değeri 97'dir.
+while c <= ord('z'): 
+      print (chr(c))
+      #chr() fonksiyonu, belirtilen Unicode veya ASCII değerine karşılık gelen karakteri döndürür
+      c = c+1
